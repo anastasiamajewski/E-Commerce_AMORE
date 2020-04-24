a83acca9d1b1a46a45e6acf1c3ebc3396e9902ed
anastasiamajewski <anastasiamajewski1991@gmail.com>
2020-04-24 14:28:24 +0200

    Add header

---
+++ frontend/src/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components/macro'
+
+export default function Header() {
+return (
+<>
+
+<HeaderStyled>
+    <LinkStyled exact to="/">Home</LinkStyled>
+    <LinkStyled to="/products">Produktdetails</LinkStyled>
+    <LinkStyled to="/shoppingcart">Warenkorb</LinkStyled>
+    <LinkStyled to="/order">Bestellung</LinkStyled>
+</HeaderStyled>
+
+</>
+)}
+
+const HeaderStyled = styled.header`
+display: flex;
+background: #964F4C;
+color: white;
+text-align:center;
+justify-content: space-around;
+`
+
+const LinkStyled = styled(NavLink)`
+display: flex;
+border-style: none;
+padding: 10px;
+background-color: #964F4C;
+height: 48px;
+color: white;
+font-size: 0.9em;
+text-decoration: underline;
+align-items: center;
+
+&.selected {
+    background: #69b0b3;
+    color: black;
+}
+
+`
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import productRoutes from "./product-api"
 import categoryRoutes from "./category-api"
 import shoppingcartRoutes from "./shoppingcart"
+import orderRoutes from "./order"
 
 const server = express()
 server.use(bodyParser.json({extended: true}));
@@ -11,5 +12,6 @@ server.use(bodyParser.urlencoded({extended: true}));
 server.use("/products", productRoutes)
 server.use("/categories", categoryRoutes)
 server.use("/shoppingcart", shoppingcartRoutes)
+server.use("/order", orderRoutes)
 
 server.listen(8080);
