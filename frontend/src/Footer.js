405df27ea25a3b4839b39b2cb692e91fcbbe2738
anastasiamajewski <anastasiamajewski1991@gmail.com>
2020-04-26 18:18:05 +0200

    Add footer and header changes

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,18 +1,43 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components/macro'
 
-export default function Footer(){
-    return(
-        <FooterStyled>
-            &copy; AMORE GmbH
-        </FooterStyled>
-    )
-}
-
-const FooterStyled = styled.footer`
-  background-color: #964F4C;
-  color: white;
-  display: flex;
-  align-items: end;
-  text-align: center;
-`
+export default function Footer() {
+    return (
+    <>
+    
+    <FooterStyled>
+        <LinkStyled exact to="/">Home</LinkStyled>
+        <LinkStyled to="/productdetails">Produktdetails</LinkStyled>
+        <LinkStyled to="/shoppingcart">Warenkorb</LinkStyled>
+        <LinkStyled to="/order">Bestellung</LinkStyled>
+    </FooterStyled>
+    
+    </>
+    )}
+    
+    const FooterStyled = styled.footer`
+    display: flex;
+    background: #964F4C;
+    color: white;
+    text-align:center;
+    justify-content: space-around;
+    `
+    
+    const LinkStyled = styled(NavLink)`
+    display: flex;
+    border-style: none;
+    padding: 10px;
+    background-color: #964F4C;
+    height: 48px;
+    color: white;
+    font-size: 0.9em;
+    text-decoration: underline;
+    align-items: center;
+    
+    &.selected {
+        background: #69b0b3;
+        color: black;
+    }
+    
+    `
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,43 +1,18 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import styled from 'styled-components/macro'
 
-export default function Header() {
-return (
-<>
-
-<HeaderStyled>
-    <LinkStyled exact to="/">Home</LinkStyled>
-    <LinkStyled to="/productdetails">Produktdetails</LinkStyled>
-    <LinkStyled to="/shoppingcart">Warenkorb</LinkStyled>
-    <LinkStyled to="/order">Bestellung</LinkStyled>
-</HeaderStyled>
-
-</>
-)}
-
-const HeaderStyled = styled.header`
-display: flex;
-background: #964F4C;
-color: white;
-text-align:center;
-justify-content: space-around;
-`
-
-const LinkStyled = styled(NavLink)`
-display: flex;
-border-style: none;
-padding: 10px;
-background-color: #964F4C;
-height: 48px;
-color: white;
-font-size: 0.9em;
-text-decoration: underline;
-align-items: center;
-
-&.selected {
-    background: #69b0b3;
-    color: black;
+export default function Header(){
+    return(
+        <HeaderStyled>
+            &copy; AMORE GmbH
+        </HeaderStyled>
+    )
 }
 
+const HeaderStyled = styled.header`
+  background-color: #964F4C;
+  color: white;
+  display: flex;
+  align-items: end;
+  text-align: center;
 `
